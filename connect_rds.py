b11fb7ff1ecaa3ae62b15bf6016b0f98881fbba3
S YOGESH <siddapati.yogesh26@gmail.com>
2025-10-28 10:02:16 +0530

    Update connect_rds.py

--- connect_rds.py
+++ connect_rds.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 conn = mysql.connector.connect(
-    host="your-rds-endpoint",
+    host="elevate.cli40464cqa5.eu-north-1.rds.amazonaws.com",
     user="admin",
     password="Kvmk26112003",
     database="demo"
